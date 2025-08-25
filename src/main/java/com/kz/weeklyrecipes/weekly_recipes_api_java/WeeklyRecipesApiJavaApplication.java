390308952d536b204eef1e1cbdf69a2d5bfe3113
天川 兄三 <kzrua8@gmail.com>
2025-08-25 11:51:38 +0900

    Initial commit: Spring Boot backend setup

---
+++ src/main/java/com/kz/weeklyrecipes/weekly_recipes_api_java/WeeklyRecipesApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.kz.weeklyrecipes.weekly_recipes_api_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyRecipesApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyRecipesApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kz/weeklyrecipes/weekly_recipes_api_java/WeeklyRecipesApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kz.weeklyrecipes.weekly_recipes_api_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyRecipesApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
