8b86bdf566a788e26720e27c6c97c5aa49fcc84e
天川 兄三 <kzrua8@gmail.com>
2025-08-30 16:10:48 +0900

    fixed test

--- src/test/java/com/kz/weeklyrecipes/WeeklyRecipesApiJavaApplicationTests.java
+++ src/test/java/com/kz/weeklyrecipes/WeeklyRecipesApiJavaApplicationTests.java
@@ -2,12 +2,11 @@ package com.kz.weeklyrecipes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WeeklyRecipesApiJavaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() { }
 }
